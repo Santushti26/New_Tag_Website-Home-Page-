80028bf7882d317e9418e15d39e0c06d3ccc6c53
Santushti 26 <91973710+Santushti26@users.noreply.github.com>
2022-09-30 09:54:26 +0530

    Update OurSponsers.js

--- src/OurSponsers.js
+++ src/OurSponsers.js
@@ -44,9 +44,13 @@ With customers in 80+ countries across 6 continents, 6D is a global organisation
 
 As a proactive technology partner, 6D works with its customers to understand market trends and business vision, developing the solutions they need to thrive in an ever-changing world." image="Assets/333.png"/> 
       <OurTeamLeft name="MUSIC BRIDGE"  body="Bridge Music exists to discover and empower the raw, passionate talent of India by providing a platform for them to collaborate with already established artists to create music that leaves a legacy true to India's creative inheritance." image="Assets/444.png"/>
-      <OurTeamLeft name="HARSH IMPEX"  body="Before opening our firm – “Harsh Impex” in the year 2009, we have been engaged in cultivation and farming since last four generations, we have our own farms in Mahuva regions of Gujarat – India which is world famous for its Onion and Cotton Farming suitability. We also have Organic Farming practices for our Pomegranate farm.
+      <OurTeamRight name="HARSH IMPEX"  body="Harsh Impex and Ishaan Plastics (P) Ltd are the Indian group of Company Manufacturing PET fridge bottles, jars, containers, perform and caps situated in Kolkata.
 
-We offer a gamut of products that encompasses Cultivating, Processing, Trading and Exporting of Fresh & Dehydrated Vegetables like Onion, Garlic along with Raw Cotton Shanker -6. We can also provide Pomegranate especially cultivated at our Nature Friendly organic farm. The above said products are pure, with high nutritional value and freshness." image="Assets/222.png"/>
+Both the Companies have Modern setup adjacent to each other in an Industrial complex having latest version of sophisticated Blow / Injection machines and Auxiliaries.
+
+The R and D wings and Quality control are of International standard.
+
+The Companies have more than 200 dedicated highly qualified professional technicians." image="Assets/222.png"/>
       <Footer/>
     
     </>
